7d90420aa0e8fb47cbb6e5b93063804131d56069
Nils Knäpper <NilsK@MacBook-Air.local>
2022-05-16 01:00:45 +0200

    working calculator

--- index.js
+++ index.js
@@ -2,49 +2,6 @@
 
 const buttons = document.querySelectorAll(".button");
 const display = document.querySelector(".display")
-function add(x, y) {
-    return x + y;
-    
-}
-
-function subtract() {
-    return x - y;    
-}
-
-function multiply() {
-    return x * y;    
-}
-
-function divide(x,y) {
-    if (y === 0)
-    { return "error" } 
-    return x / y;
-}
-
-function operate(x , y, operator) {
-    if (operator === "/")
-    {
-        let output = divide(x, y);
-        if (output === "error") {
-            return "Divide by 0";
-        }
-        else {
-            return output;
-        }
-    }
-    if (operator === "*")
-    {
-        return multiply(x,y)
-    }
-    if (operator === "-")
-    {
-        return   subtract(x,y)
-    } 
-    if (operator === "+")
-    {
-        return add(x,y)
-    }   
-}
 
 function del() {
     const content = display.textContent;   
@@ -54,20 +11,86 @@ function del() {
 function clear() {
     display.textContent = "";
 }
-function splitInput(inputText) {
-    
-    let operators = ["+", "-", "/", "*"];
 
-    for (let i = 0; i < Array.length; i++){
-        let operatorPos = inputText.indexOf(operators[i]);
-        if (operatorPos >= 0) {
-            let output = [inputText.substring(0, operatorPos), inputText.substring(operatorPos + 1, inputText.length), operators[i]];
-            return output;
+
+function operate(displayText) {
+
+    let calcChain = [];
+    const operators = ["+", "*", "-", "/"];
+
+    function splitString(text) {
+    return text.split(/([{\+\-\/*])/);
+    }
+
+    function add(num1, num2)
+    {
+    return num1 + num2;
+    }
+    function substract(num1, num2)
+    {
+    return num1 - num2;
+    }
+    function multiply(num1, num2) {
+    return num1 * num2;
+    }
+    function divide(num1, num2) {
+    return num1 / num2;
+    }
+
+    function pointCalc(calcChain) {
+    for (let pos = 0; pos < calcChain.length; pos++){
+        const elem = calcChain[pos]
+        if (operators.includes(elem)) {
+            const num1 = calcChain[+pos - 1];
+            const num2 = calcChain[+pos + 1];
+            if ((elem === "/") || (elem === "*")) {
+                if (elem === "/") {
+                    const differential = divide(+num1, +num2);
+                    calcChain[pos - 1] = differential;
+                }
+                else if (elem === "*") {
+                    const product = multiply(+num1, +num2);
+                    calcChain[pos - 1] = product;
+                }
+                calcChain.splice(+pos, 2)
+                pos -= pos;
+            }
         }
     }
+    }
+
+    function dashCalc(calcChain) {
+    for (let pos = 0; pos < calcChain.length; pos++){
+        const elem = calcChain[pos]
+        if (operators.includes(elem)) {
+            const num1 = calcChain[+pos - 1]
+            const num2 = calcChain[+pos + 1]
+            if ((elem === "+") || (elem === "-")){
+        
+                if (elem === "+") {
+                    const sum = add(+num1, +num2);
+                    calcChain[pos - 1] = sum;
+                }
+                else if (elem === "-") {
+                    const substraction = substract(+num1, +num2);
+                    calcChain[pos - 1] = substraction;
+                }
+                calcChain.splice(+pos, 2)
+                pos -= pos;
+            
+            }   
+        }
+    }
+    }
+    calcChain = splitString(displayText);
+    console.log(calcChain);
+    while (calcChain.length > 1) {
+        pointCalc(calcChain);
+        dashCalc(calcChain);
+    }
+    return calcChain[0];
+
 }
-
-
 
 
 
@@ -83,9 +106,7 @@ buttons.forEach( (button) => {
         }
         else if (buttonText === "=") {
 
-            let output = splitInput(display.textContent.trim()); 
-            console.log(typeof(output))
-            display.textContent = output[0];
+            display.textContent = operate(display.textContent.trim()).toFixed(2); 
         }
         else {
             display.textContent +=buttonText;           
